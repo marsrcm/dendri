b7a074976cbde09a2a1f89763e89bffbc91fa9bc
Lukas Loidolt <mail@lukas-loidolt.at>
2026-08-16 23:47:47 +0200

    rename lib.nix to core.nix

--- flake.nix
+++ flake.nix
@@ -2,6 +2,6 @@
   description = "Small custom implementation of a dendritic module system for my own personal configs";
 
   outputs = {
-    mkLib = import ./lib;
+    mkLib = import ./core;
  };
 }
