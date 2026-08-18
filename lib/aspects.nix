26ead4d5c15521571623a30bb13bfe077bab6da4
Lukas Loidolt <mail@lukas-loidolt.at>
2026-08-18 14:08:42 +0200

    allow nested aspect trees

--- lib/aspects.nix
+++ lib/aspects.nix
@@ -3,16 +3,9 @@
   resolveAspect,
 }:
 let
-  aspectType = lib.types.submodule (
+  aspectSubmodule = lib.types.submodule (
     { name, ... }: {
       options = {
-        _name = lib.mkOption {
-          type = lib.types.str;
-          default = name;
-          internal = true;
-          readOnly = true;
-        };
-
         includes = lib.mkOption {
           # TODO: try changing this type to aspectType
           type = lib.types.listOf lib.types.raw;
@@ -32,9 +25,34 @@ let
     }
   );
 
+
+  isAspect = value: lib.isAttrs value && (value ? includes || value ? nixos || value ? homeManager);
+
+  aspectType = lib.types.mkOptionType {
+    name = "aspect";
+    description = "aspect";
+    check = isAspect;
+    merge = loc: defs:
+      (aspectSubmodule.merge loc defs)
+      // {
+        _name = lib.concatStringsSep "." (lib.drop 1 loc);
+      };
+    inherit (aspectSubmodule) getSubOptions getSubModules substSubModules;
+  };
+
+
+  aspectTree =
+    lib.types.addCheck
+      (lib.types.attrsOf (lib.types.either aspectType aspectTree))
+      (value: !isAspect value)
+    // {
+    description = "Aspect Tree";
+    descriptionClass = "noun";
+  };
+
   aspectsModule = {
     options.aspects = lib.mkOption {
-      type = lib.types.lazyAttrsOf aspectType;
+      type = aspectTree;
       default = { };
     };
   };
@@ -48,10 +66,27 @@ let
       ++ aspectModules;
     }).config.aspects;
 
-  modulesFromAspects = aspects: {
-    nixos = lib.mapAttrs (_: aspect: resolveAspect "nixos" aspect) aspects;
-    homeManager = lib.mapAttrs (_: aspect: resolveAspect "homeManager" aspect) aspects;
-  };
+  flattenAspects =
+    let
+      walk = acc: node:
+        if node ? _name then
+          acc // { ${node._name} = node; }
+        else
+          lib.foldl'
+            (acc': name: walk acc' node.${name})
+            acc
+            (builtins.attrNames node);
+    in
+      walk { };
+
+  modulesFromAspects = aspects:
+    let
+      flattenedAspects = flattenAspects aspects;
+    in
+      {
+        nixos = lib.mapAttrs (_: aspect: resolveAspect "nixos" aspect) flattenedAspects;
+        homeManager = lib.mapAttrs (_: aspect: resolveAspect "homeManager" aspect) flattenedAspects;
+      };
 in
 {
   inherit evalAspects modulesFromAspects;
